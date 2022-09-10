92ff8b6094b29619db1d1d8ef635d3884935846e
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2022-09-10 18:48:37 -0300

    adjusts gradle automation

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     implementation("io.cucumber:cucumber-java:7.7.0")
     testImplementation("io.cucumber:cucumber-junit:7.7.0")
+    testImplementation("org.junit.vintage:junit-vintage-engine:5.9.0")
+
 
 
 }
--- src/test/java/org/memes/RunCucumberTest.java
+++ src/test/java/org/memes/RunCucumberTest.java
@@ -5,6 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources"
+)
 public class RunCucumberTest{
 }
